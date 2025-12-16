f3e7ff2cd218bbd751381e8f4417763f375be31b
Hexolar <pixtrixtrick@gmail.com>
2025-12-16 09:26:21 +0200

    kaut kas!

--- index.php
+++ index.php
@@ -2,6 +2,63 @@
 require_once "back-end.php";
 require_once "database.php";
 
-$gifts = gifts_to_array(18);
-echo "Dāvanu saraksts: " . $gifts;
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+    <link href='https://fonts.googleapis.com/css?family=Josefin Sans' rel='stylesheet'>
+</head>
+<body>
+<div class="min-h-screen max-w-full">
+    <main>
+        <div class="title-box">
+            <div class="title">
+                Vēstule Ziemassvētku vecītim
+            </div>
+            <div class="small-title">
+                Lai Tavas vēlmes piepildās!
+            </div>
+        </div>
+    </main>
+    <main>
+        <div class="tutorial-box">
+            <div class="image-box">
+                <div class="number">1.</div>
+                <div class="image"><img src="http://konkurss.proti.lv/images/r22/1write.svg"></div>
+            </div>
+            <div class="tutorial-title">Uzraksti vēstuli</div>
+        </div>
+        <div class="tutorial-box">
+            <div class="image-box">
+                <div class="number">2.</div>
+                <div class="image"><img src="https://konkurss.proti.lv/images/r22/2choose.svg"></div>
+            </div>
+            <div class="tutorial-title">Izvēlies dāvanas</div>
+        </div>
+        <div class="tutorial-box">
+            <div class="image-box">
+                <div class="number">3.</div>
+                <div class="image"><img src="https://konkurss.proti.lv/images/r22/3send.svg"></div>
+            </div>
+            <div class="tutorial-title">Nosūti vecītim</div>
+        </div>
+    </main>
+    <main>
+    <div class="title-box">
+            <div class="number">
+            1.
+            </div>
+            <div class="first-title">
+                Vēstule Ziemassvētku vecītim
+            </div>
+            
+        </div>
+    </main>
+</div>
+</body>
+</html>
